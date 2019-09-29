cd6a6a96a0db276164ab35f6f34ce2b49fff425d
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-09-29 19:45:59 +0200

    Add files via upload

--- ITT/segmentation.py
+++ ITT/segmentation.py
@@ -5,7 +5,7 @@ import cv2
 from matplotlib import pyplot as plt
 
 # Image operation using thresholding
-img = cv2.imread('fg.jpg')
+img = cv2.imread('images.jpeg')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -19,9 +19,8 @@ fg = cv2.erode(thresh,None,iterations = 2)
 cv2.imshow('image', fg) # foreground
 '''
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-'''
+
+
 # noise removal
 kernel = np.ones((3,3),np.uint8)
 opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
@@ -32,8 +31,7 @@ dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
 ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
 # Finding unknown region
 sure_fg = np.uint8(sure_fg)
-unknown = cv2.subtract(sure_bg,sure_fg)
-cv2.imshow('image', sure_fg) # foreground
+unknown = cv2.subtract(sure_bg,sure_bg)
+cv2.imshow('image', sure_bg) # foreground
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-'''
