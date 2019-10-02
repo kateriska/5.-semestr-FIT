d85b9c1b40194885c29e615adf343845c3525609
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-10-02 20:15:47 +0200

    Add files via upload

--- ITT/orFieldEst.py
+++ ITT/orFieldEst.py
@@ -1,41 +1,33 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
-from PIL import Image, ImageDraw
+#from matplotlib import pyplot as plt
+
 
 img = cv2.imread("nwmPa.png", cv2.IMREAD_COLOR)  # uint8 image
 #norm_image = cv2.normalize(image, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 norm_image = cv2.normalize(img,None,0,255,cv2.NORM_MINMAX)
-cv2.imshow('Result', norm_image)
+cv2.imshow('NormImage', norm_image)
 
 height = np.size(norm_image, 0)
 width = np.size(norm_image, 1)
 print(height)
 print(width)
 
-step_count = 16
-image = Image.new(mode='L', size=(height, width), color=255)
+step = 16
+x = 16
+y = 16
 
-# Draw some lines
-draw = ImageDraw.Draw(image)
-y_start = 0
-step_size = int(image.width / step_count)
+while x < width:
+    cv2.line(norm_image, (x, 0), (x, height), color=(0, 0, 0), lineType=cv2.LINE_AA, thickness=1)
+    x += step
 
-for x in range(0, image.width, step_size):
-    line = ((x, y_start), (x, height))
-    draw.line(line, fill=128)
+while y < height:
+    cv2.line(norm_image, (0, y), (width, y), color=(0, 0, 0), lineType=cv2.LINE_AA, thickness=1)
+    y += step
 
-x_start = 0
+cv2.imshow('Norm Image With Grid', norm_image)
 
-for y in range(0, image.height, step_size):
-    line = ((x_start, y), (width, y))
-    draw.line(line, fill=128)
 
-del draw
-
-image.show()
-result = cv2.add(image, img)
-cv2.imshow('Result', result)
 
 
 
