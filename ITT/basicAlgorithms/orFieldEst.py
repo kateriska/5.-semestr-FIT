da9284cac6ff60c6f1e2e1e3bb454f3bc33ab681
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-10-08 12:34:49 +0200

    Add files via upload

---
+++ ITT/basicAlgorithms/orFieldEst.py
@@ -0,0 +1,89 @@
+import numpy as np
+import cv2
+import math
+
+img = cv2.imread("104_1.tif", 0)  # uint8 image
+
+# normalize image
+norm_image = cv2.normalize(img,None,0,255,cv2.NORM_MINMAX)
+cv2.imshow('NormImage', norm_image)
+
+rows = np.size(norm_image, 0)
+cols = np.size(norm_image, 1)
+print(rows)
+print(cols)
+
+step = 16
+x = 16
+y = 16
+
+while x < rows:
+    cv2.line(norm_image, (x, 0), (x, rows), color=(0, 0, 0), lineType=cv2.LINE_AA, thickness=1)
+    x += step
+
+while y < cols:
+    cv2.line(norm_image, (0, y), (cols, y), color=(0, 0, 0), lineType=cv2.LINE_AA, thickness=1)
+    y += step
+
+cv2.imshow('Norm Image With Grid', norm_image)
+
+block_div = 8
+size_img = norm_image.shape;
+
+print(norm_image.shape)
+print(norm_image.dtype)
+
+grad_x = np.zeros(size_img, dtype=np.uint8)
+grad_y = np.zeros(size_img, dtype=np.uint8)
+Vx = np.zeros(size_img, dtype=np.uint8)
+Vy = np.zeros(size_img, dtype=np.uint8)
+theta = np.zeros(size_img, dtype=np.uint8)
+
+#compute gradients for each pixel
+grad_x = cv2.Sobel(norm_image,cv2.CV_64F,1,0,ksize=5)
+grad_y = cv2.Sobel(norm_image,cv2.CV_64F,0,1,ksize=5)
+
+for i in range(block_div, rows-block_div, step):
+    for j in range(block_div, cols-block_div, step):
+        sum_Vx = 0.0
+        sum_Vy = 0.0
+        for u in range(i-block_div, i+block_div):
+            for v in range(j-block_div, j+block_div):
+                sum_Vy = sum_Vy + (2 * grad_x[u][v] * grad_y[u][v])
+                sum_Vx = sum_Vx + (grad_x[u][v]**2 - grad_y[u][v]**2)
+                #print(sum_Vx)
+                #print(sum_Vy)
+
+        Vy[i][j] = sum_Vy
+        Vx[i][j] = sum_Vx
+        theta_calculation = 0.0
+
+        if (sum_Vx != 0 ):
+            theta_calculation = 0.5 * math.atan(sum_Vy / sum_Vx)
+            print(theta_calculation)
+
+        theta[i][j] = theta_calculation
+
+
+
+
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
