9f60a863be3acf9ee3c640e68ab552a6a4c0482e
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-10-26 12:24:55 +0200

    Add files via upload

--- ITT/basicAlgorithms/or.py
+++ ITT/basicAlgorithms/or.py
@@ -2,16 +2,29 @@ import numpy as np
 import cv2
 import math
 
-img = cv2.imread("104_1.tif")  # uint8 image
-print(img.dtype)
-img = np.int16(img)
+orig_img = cv2.imread("104_1.tif")  # uint8 image
+print(orig_img.dtype)
+img = np.int16(orig_img)
 print(img.dtype)
 
 rows = np.size(img, 0)
 cols = np.size(img, 1)
 print(rows)
 print(cols)
+print(np.size(img, 2))
+step = 16
+x = 16
+y = 16
+# draw grid for squares 16 x 16
+while x < rows:
+    cv2.line(img, (x, 0), (x, rows), color=(0, 0, 0), lineType=cv2.LINE_AA, thickness=1)
+    x += step
 
+while y < cols:
+    cv2.line(img, (0, y), (cols, y), color=(0, 0, 0), lineType=cv2.LINE_AA, thickness=1)
+    y += step
+
+cv2.imshow('Image With Grid', img)
 shape_img = img.shape
 
 grad_x = np.zeros(shape_img, dtype=np.int16)
@@ -24,7 +37,7 @@ grad_x = cv2.Sobel(img,cv2.CV_16SC1,1, 0)
 grad_y = cv2.Sobel(img,cv2.CV_16SC1,0, 1)
 
 block_div = 8
-step = 16
+right_angle  = math.pi / 2
 
 for i in range(block_div, rows-block_div, step):
     for j in range(block_div, cols-block_div, step):
@@ -49,6 +62,8 @@ for i in range(block_div, rows-block_div, step):
                 sum_Vy = sum_Vy + (grad_x_value**2 * grad_y_value**2)
 
         #print(sum_Vx)
+        Vy[i][j] = sum_Vy
+        Vx[i][j] = sum_Vx
 
         theta_calculation = 0.0
 
@@ -59,3 +74,30 @@ for i in range(block_div, rows-block_div, step):
             theta_calculation = 0.0
 
         print(theta_calculation)
+        magnitude = math.sqrt((sum_Vx * sum_Vx) + (sum_Vy * sum_Vy))
+        phi_x = magnitude * math.cos(2*theta_calculation)
+        phi_y = magnitude * math.sin(2*theta_calculation)
+
+        orientation = 0.0
+        if (phi_x != 0):
+            orientation = 0.5 * math.atan(phi_y / phi_x)
+            #print(orientation)
+        else:
+            orientation = 0.0
+
+        X0 = i + 8
+        Y0 = j + 8
+        r = 8
+
+        X1 = r*math.cos(orientation - right_angle)+X0
+        X1 = int (X1)
+        print(X1)
+
+        Y1 = r*math.sin(orientation - right_angle)+Y0
+        Y1 = int (Y1)
+
+        orient_img = cv2.line(orig_img,(X0,Y0) , (X1,Y1), (0,255,0), 3)
+        cv2.imshow('Oriented image', orient_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
