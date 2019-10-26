203a7a91a0aabe56c0c3b65a1eacdef0c00567f6
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-10-26 18:31:29 +0200

    Add files via upload

--- ITT/basicAlgorithms/bp.py
+++ ITT/basicAlgorithms/bp.py
@@ -44,21 +44,24 @@ def imgThinning(img):
     return skeleton
 
 def orientFieldEstimation(orig_img):
-    img = np.int16(orig_img)
+    img = np.float32(orig_img)
 
     rows = np.size(img, 0)
     cols = np.size(img, 1)
 
     shape_img = img.shape
 
-    grad_x = np.zeros(shape_img, dtype=np.int16)
-    grad_y = np.zeros(shape_img, dtype=np.int16)
-    Vx = np.zeros(shape_img, dtype=np.int16)
-    Vy = np.zeros(shape_img, dtype=np.int16)
-    theta = np.zeros(shape_img, dtype=np.int16)
+    grad_x = np.zeros(shape_img, dtype=np.float32)
+    grad_y = np.zeros(shape_img, dtype=np.float32)
+    Vx = np.zeros(shape_img, dtype=np.float32)
+    Vy = np.zeros(shape_img, dtype=np.float32)
+    theta = np.zeros(shape_img, dtype=np.float32)
+    phi_x_array = np.zeros(shape_img, dtype=np.float32)
+    phi_y_array = np.zeros(shape_img, dtype=np.float32)
+    magnitude_array = np.zeros(shape_img, dtype=np.float32)
 
-    grad_x = cv2.Sobel(img,cv2.CV_16SC1,1, 0)
-    grad_y = cv2.Sobel(img,cv2.CV_16SC1,0, 1)
+    grad_x = cv2.Sobel(img,cv2.CV_32FC1,1, 0)
+    grad_y = cv2.Sobel(img,cv2.CV_32FC1,0, 1)
 
     block_div = 8
     right_angle  = math.pi / 2
@@ -70,30 +73,31 @@ def orientFieldEstimation(orig_img):
             sum_Vy = 0.0
             for u in range(i-block_div, i+block_div):
                 for v in range(j-block_div, j+block_div):
+                    #print(grad_x[u][v])
                     grad_x_value_np = grad_x[u][v]
                     grad_x_value_str = np.array2string(grad_x_value_np)
-                    grad_x_value_str = grad_x_value_str.split(" ", 1)[0]
-                    grad_x_value_str = grad_x_value_str[1:]
+                    grad_x_value_str = grad_x_value_str.split("[", 1)[1]
+                    grad_x_value_str = grad_x_value_str.split(".", 1)[0]
+                    grad_x_value_str = grad_x_value_str.strip();
                     grad_x_value = int (grad_x_value_str)
 
                     grad_y_value_np = grad_y[u][v]
                     grad_y_value_str = np.array2string(grad_y_value_np)
-                    grad_y_value_str = grad_y_value_str.split(" ", 1)[0]
-                    grad_y_value_str = grad_y_value_str[1:]
+                    grad_y_value_str = grad_y_value_str.split("[", 1)[1]
+                    grad_y_value_str = grad_y_value_str.split(".", 1)[0]
+                    grad_y_value_str = grad_y_value_str.strip();
                     grad_y_value = int (grad_y_value_str)
 
-                    sum_Vx = sum_Vx + (2 * grad_x_value * grad_y_value)
-                    sum_Vy = sum_Vy + (grad_x_value**2 * grad_y_value**2)
 
-
-            theta_calculation = 0.0
+                    sum_Vy = sum_Vy + (2 * grad_x_value * grad_y_value)
+                    sum_Vx = sum_Vx + ((grad_x_value**2) - (grad_y_value**2))
 
             if (sum_Vx != 0 ):
                 theta_calculation = 0.5 * math.atan(sum_Vy / sum_Vx)
             else:
                 theta_calculation = 0.0
 
-            print(theta_calculation)
+            #print(theta_calculation)
             magnitude = math.sqrt((sum_Vx * sum_Vx) + (sum_Vy * sum_Vy))
             phi_x = magnitude * math.cos(2*theta_calculation)
             phi_y = magnitude * math.sin(2*theta_calculation)
@@ -105,24 +109,30 @@ def orientFieldEstimation(orig_img):
             else:
                 orientation = 0.0
 
-            X0 = i + 8
-            Y0 = j + 8
+            print(orientation)
+            X0 = i
+            Y0 = j
             r = 8
 
-            X1 = r*math.cos(orientation - right_angle)+X0
+            X1 = r*math.cos(theta_calculation - right_angle)+X0
             X1 = int (X1)
-            print(X1)
+            #print(X1)
 
-            Y1 = r*math.sin(orientation - right_angle)+Y0
+            Y1 = r*math.sin(theta_calculation - right_angle)+Y0
             Y1 = int (Y1)
 
             orient_img = cv2.line(orig_img,(X0,Y0) , (X1,Y1), (0,255,0), 3)
             cv2.imshow('Oriented image', orient_img)
 
+
+
+
+
     return orient_img
 
 
-img = cv2.imread("104_1.tif",0) # uint8 image
+img = cv2.imread("101_2.tif",0) # uint8 image
+img = cv2.resize(img,(388,374))
 img = cv2.normalize(img,None,0,255,cv2.NORM_MINMAX)
 cv2.imshow('Original uint8 image', img)
 segmented_image = imgSegmentation(img)
