974834d8a551882d6da0b4df558b828fdd66e91e
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-10-28 13:44:40 +0100

    Add files via upload

--- ITT/basicAlgorithms/gabor.py
+++ ITT/basicAlgorithms/gabor.py
@@ -3,14 +3,27 @@ import cv2
 from matplotlib import pyplot as plt
 import math
 
-orig_img = cv2.imread("segmented_img.tif")  # uint8 image
+orig_img = cv2.imread("104_1.tif")  # uint8 image
 img = np.float32(orig_img)
 shape_img = img.shape
 
-kernel1 = cv2.getGaborKernel((9,9), 5, 0, 8, 0.02, 0, cv2.CV_32F);
-kernel2 = cv2.getGaborKernel((9,9), 5, 45, 8, 0.02, 0, cv2.CV_32F);
-kernel3 = cv2.getGaborKernel((9,9), 5, 90, 8, 0.02, 0, cv2.CV_32F);
-kernel4 = cv2.getGaborKernel((9,9), 5, 135, 8, 0.02, 0, cv2.CV_32F);
+kernel_size = (12,12)
+sigma = 6
+lambda_v = 10
+gamma = 0.05
+psi = 0
+
+theta1 = 0
+theta2 = 45
+theta3 = 90
+theta4 = 135
+
+enhanced = np.zeros(shape_img, dtype=np.float32)
+
+kernel1 = cv2.getGaborKernel(kernel_size, sigma, theta1, lambda_v, gamma, psi, cv2.CV_32F);
+kernel2 = cv2.getGaborKernel(kernel_size, sigma, theta2, lambda_v, gamma, psi, cv2.CV_32F);
+kernel3 = cv2.getGaborKernel(kernel_size, sigma, theta3, lambda_v, gamma, psi, cv2.CV_32F);
+kernel4 = cv2.getGaborKernel(kernel_size, sigma, theta4, lambda_v, gamma, psi, cv2.CV_32F);
 gabor1 = cv2.filter2D(img, -1, kernel1);
 gabor2 = cv2.filter2D(img, -1, kernel2);
 gabor3 = cv2.filter2D(img, -1, kernel3);
@@ -22,8 +35,21 @@ cv2.imshow('gabor4', gabor4)
 adding1 = cv2.add(gabor1, gabor2)
 adding2 = cv2.add(adding1, gabor3)
 adding3 = cv2.add(adding2, gabor4)
-#cv2.imshow('sym', adding1)
+cv2.imshow('sym', adding1)
+cv2.imshow('sym2', adding2)
 cv2.imshow('final', adding3)
-cv2.imwrite('gabor.tif', adding3)
+cv2.imwrite('gabor.tif', adding2)
+
+cv2.addWeighted(enhanced , 0, gabor1, 1, 0, enhanced );
+cv2.addWeighted(enhanced , 1, gabor2, 1, 0, enhanced );
+cv2.addWeighted(enhanced , 1, gabor3, 1, 0, enhanced );
+cv2.addWeighted(enhanced , 1, gabor4, 1, 0, enhanced );
+
+#gblur = cv2.GaussianBlur(adding3, (5, 5), 0)
+#cv2.imshow('blur', gblur)
+#cv2.imwrite('blur.tif', gblur)
+#gblur_img = cv2.imread("blur.tif",0)  # uint8 image
+#ret, result = cv2.threshold(gblur_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU) # transform to binary image
+cv2.imshow('result', enhanced)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- ITT/basicAlgorithms/minutae.py
+++ ITT/basicAlgorithms/minutae.py
@@ -3,9 +3,11 @@ import cv2
 from matplotlib import pyplot as plt
 import math
 
-img = cv2.imread("thinned_img.tif",0)  # uint8 image
+img = cv2.imread("thinned_gabor.tif",0)  # uint8 image
+
 ret, tresh_img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 cv2.imshow("Inverse treshold", tresh_img)
+
 rows = np.size(tresh_img, 0)
 cols = np.size(tresh_img, 1)
 a=0
@@ -31,10 +33,80 @@ for x in range(0,rows-1):
         pix5 = tresh_img[x][y]
         pix6 = tresh_img[x+1][y]
         pix7 = tresh_img[x-1][y+1]
-        print(pix7)
+        print(pix5)
         pix8 = tresh_img[x][y+1]
         pix9 = tresh_img[x+1][y+1]
 
+        if(pix1 == 255 and pix2 == 0 and pix3 == 0 and pix4 == 0 and pix5 == 255 and pix6 == 0 and pix7 == 0 and pix8==0 and pix9==0):
+            ridge = ridge + 1
+            ridcheck = ridcheck + 1
+        if(pix1==0 and pix2==255 and pix3==0 and pix4==0 and pix5==255 and pix6==0 and pix7==0 and pix8==0 and pix9==0):
+            ridge = ridge + 1
+            ridcheck = ridcheck + 1
+        if (pix1==0 and pix2==0 and pix3==255 and pix4==0 and pix5==255 and pix6==0 and pix7==0 and pix8==0 and pix9==0):
+            ridge = ridge + 1
+            ridcheck = ridcheck + 1
+        if (pix1==0 and pix2==0 and pix3==0 and pix4==255 and pix5==255 and pix6==0 and pix7==0 and pix8==0 and pix9==0):
+            ridge = ridge + 1
+            ridcheck = ridcheck + 1
+        if (pix1==0 and pix2==0 and pix3==0 and pix4==0 and pix5==255 and pix6==255 and pix7==0 and pix8==0 and pix9==0):
+            ridge = ridge + 1
+            ridcheck = ridcheck + 1
+        if (pix1==0 and pix2==0 and pix3==0 and pix4==0 and pix5==255 and pix6==0 and pix7==255 and pix8==0 and pix9==0):
+            ridge = ridge + 1
+            ridcheck = ridcheck + 1
+        if (pix1==0 and pix2==0 and pix3==0 and pix4==0 and pix5==255 and pix6==0 and pix7==0 and pix8==255 and pix9==0):
+            ridge = ridge + 1
+            ridcheck = ridcheck + 1
+        if (pix1==0 and pix2==0 and pix3==0 and pix4==0 and pix5==255 and pix6==0 and pix7==0 and pix8==0 and pix9==255):
+            ridge = ridge + 1
+            ridcheck = ridcheck + 1
+##################################################################################################################################
+        if (pix1==255 and pix2==0 and pix3==0 and pix4==0 and pix5==255 and pix6==255 and pix7==255 and pix8==0 and pix9==0):
+            bif = bif + 1
+            bifcheck = bifcheck + 1
+        if (pix1==0 and pix2==255 and pix3==0 and pix4==255 and pix5==255 and pix6==0 and pix7==0 and pix8==0 and pix9==255):
+            bif = bif + 1
+            bifcheck = bifcheck + 1
+        if (pix1==255 and pix2==0 and pix3==255 and pix4==0 and pix5==255 and pix6==0 and pix7==0 and pix8==255 and pix9==0):
+            bif = bif + 1
+            bifcheck = bifcheck + 1
+        if (pix1==0 and pix2==255 and pix3==0 and pix4==0 and pix5==255 and pix6==255 and pix7==255 and pix8==0 and pix9==0):
+            bif = bif + 1
+            bifcheck = bifcheck + 1
+        if (pix1==0 and pix2==0 and pix3==255 and pix4==255 and pix5==255 and pix6==0 and pix7==0 and pix8==0 and pix9==255):
+            bif = bif + 1
+            bifcheck = bifcheck + 1
+        if (pix1==255 and pix2==0 and pix3==0 and pix4==0 and pix5==255 and pix6==255 and pix7==0 and pix8==255 and pix9==0):
+            bif = bif + 1
+            bifcheck = bifcheck + 1
+        if (pix1==0 and pix2==255 and pix3==0 and pix4==0 and pix5==255 and pix6==0 and pix7==255 and pix8==0 and pix9==255):
+            bif = bif + 1
+            bifcheck = bifcheck + 1
+        if (pix1==0 and pix2==0 and pix3==255 and pix4==255 and pix5==255 and pix6==0 and pix7==0 and pix8==255 and pix9==0):
+            bif = bif + 1
+            bifcheck = bifcheck + 1
 
+        if (ridge > 0):
+            cv2.circle(tresh_img, (x,y), 4, (255,0,0), 2)
+            ridge = 0
+        #if (bif > 0):
+            #cv2.rectangle(tresh_img, (x-2,y-2), (x+2,y+2), (255,0,0),2)
+            #bif = 0
+
+        pix1 = 0
+        pix2 = 0
+        pix3 = 0
+        pix4 = 0
+        pix5 = 0
+        pix6 = 0
+        pix7 = 0
+        pix8 = 0
+        pix9 = 0
+
+
+print(ridcheck)
+print(bifcheck)
+cv2.imshow("Ridges", tresh_img);
 cv2.waitKey(0)
 cv2.destroyAllWindows()
