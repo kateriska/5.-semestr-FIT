085dd613f706255cefa42555befe588973f5a7fe
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-12-17 21:36:18 +0100

    Add files via upload

--- IMP/projekt/source/main.c
+++ IMP/projekt/source/main.c
@@ -1,10 +1,10 @@
 /*
-Jméno a příjmení: Kateřina Fořtová
+Jmeno a prijmeni: Katerina Fortova
 Login: xforto00
-Projekt: ARM-FITkit3: Měření srdečního tepu
-Předmět: IMP
-originál
-Datum posledních změn v souboru:
+Projekt: ARM-FITkit3: Mereni srdecniho tepu
+Predmet: IMP
+original
+Datum poslednich zmen v souboru: 17. prosince 2019
 */
 
 #include "MK60D10.h" // for FitKIT
@@ -12,16 +12,14 @@ Datum posledních změn v souboru:
 #include "pin_mux.h" // for configuration of ports
 #include "fsl_pit.h" // for Periodic interrupt timer - refreshing display
 #include "fsl_clock.h" // for work with clk
-#include "fsl_lptmr.h" // for Low-power timer - beat measurement
+#include "fsl_lptmr.h" // for Low-power timer - beat measuring
 #include "fsl_adc16.h" // for work with Analog-to-digital converter, transform analog signal from module to digital
 
-// C libraries
+// C libraries:
 #include <stdio.h>
 #include <stdbool.h>
 
-#define MEASURED_VALUES_ARR_SIZE 3 // count of measured values in array
-
-static uint32_t clk_frequency; // // frequency of clk
+static uint32_t clk_frequency; // frequency of clk
 
 static char measured_value_str[5]; // string for show final result on display
 
@@ -207,7 +205,7 @@ void PITinit()
 
 
 /*
-Function for init LPTMR for count time intervals and time during beat measurement
+Function for init LPTMR for count time intervals and time during beat measuring
 */
 void LPTMRinit()
 {
@@ -221,8 +219,7 @@ void LPTMRinit()
 	LPTMR_config.prescalerClockSource = kLPTMR_PrescalerClock_0;
 
 	LPTMR_Init(LPTMR0, &LPTMR_config);
-	static uint32_t LPTMR0_period = 0xFFFF;
-	LPTMR_SetTimerPeriod(LPTMR0, LPTMR0_period);
+	LPTMR_SetTimerPeriod(LPTMR0, 0xFFFF); // count until this value
 }
 
 
@@ -238,29 +235,30 @@ void ADCinit()
 	ADC_config.enableContinuousConversion = true;
 
 	// setting sample resolution - 16-bit
-#if (defined(FSL_FEATURE_ADC16_MAX_RESOLUTION) && FSL_FEATURE_ADC16_MAX_RESOLUTION >= 16)
+	#if ((defined(FSL_FEATURE_ADC16_MAX_RESOLUTION)) && (FSL_FEATURE_ADC16_MAX_RESOLUTION >= 16))
 	ADC_config.resolution = kADC16_Resolution16Bit;
-#endif
+	#endif
 
 	ADC16_Init(ADC0, &ADC_config);
 	ADC16_EnableHardwareTrigger(ADC0, false);
 	ADC16_SetHardwareAverage(ADC0, kADC16_HardwareAverageCount32);
 
 	// calibration of ADC
-#if (defined(FSL_FEATURE_ADC16_HAS_CALIBRATION) && FSL_FEATURE_ADC16_HAS_CALIBRATION)
+	#if ((defined(FSL_FEATURE_ADC16_HAS_CALIBRATION)) && (FSL_FEATURE_ADC16_HAS_CALIBRATION))
 	ADC16_DoAutoCalibration(ADC0);
-#endif
+	#endif
 
 	// setting channel of ADC
-	adc16_channel_config_t ADC_channel_config = {
+	adc16_channel_config_t ADC_channel_config =
+	{
 		.channelNumber = 0,
 		.enableInterruptOnConversionCompleted = false,
 	};
 
 	// use differential sample mode
-#if (defined(FSL_FEATURE_ADC16_HAS_DIFF_MODE) && FSL_FEATURE_ADC16_HAS_DIFF_MODE)
+	#if (defined(FSL_FEATURE_ADC16_HAS_DIFF_MODE) && FSL_FEATURE_ADC16_HAS_DIFF_MODE)
 	ADC_channel_config.enableDifferentialConversion = false;
-#endif
+	#endif
 
 	ADC16_SetChannelConfig(ADC0, 0, &ADC_channel_config);
 }
@@ -274,6 +272,7 @@ float signalFiltering(float dt, float x_i_lowpass)
 	// fc = 2 Hz - average fc of heartbeat
 	// 1 / (2 * pi * 2) = 0.0795774155 seconds = 79577.47155 microseconds
 	float tau = 79577.47155;
+
 	// lowpass filtering:
 	float y_i_lowpass;
 	static float y_i_preceding_lowpass = 0.0;
@@ -284,7 +283,7 @@ float signalFiltering(float dt, float x_i_lowpass)
 	static float x_i_preceding_highpass = 0.0;
 	static float y_i_preceding_highpass = 0.0;
 
-	// variable for control x_0 index
+	// variable for control x_0 index:
 	static int actual_x_index = 0;
 
 	float alpha_lowpass = dt / (tau + dt);
@@ -302,6 +301,7 @@ float signalFiltering(float dt, float x_i_lowpass)
 		actual_x_index++;
 	}
 
+
 	/*
 	 // Return RC low-pass filter output samples, given input samples,
  	 // time interval dt, and time constant RC
@@ -316,7 +316,7 @@ float signalFiltering(float dt, float x_i_lowpass)
    	   Source: https://en.wikipedia.org/wiki/Low-pass_filter
 	 */
 
-	y_i_lowpass = alpha_lowpass * x_i_lowpass + (1.0 - alpha_lowpass) * y_i_preceding_lowpass;
+	y_i_lowpass = (alpha_lowpass * x_i_lowpass) + ((1.0 - alpha_lowpass) * y_i_preceding_lowpass);
 
 	y_i_preceding_lowpass = y_i_lowpass;
 
@@ -339,7 +339,7 @@ float signalFiltering(float dt, float x_i_lowpass)
 	float alpha_highpass = tau / (tau + dt);
 
 
-	y_i_highpass = alpha_highpass * y_i_preceding_highpass + alpha_highpass * (x_i_highpass - x_i_preceding_highpass);
+	y_i_highpass = (alpha_highpass * y_i_preceding_highpass) + (alpha_highpass * (x_i_highpass - x_i_preceding_highpass));
 
 
 	x_i_preceding_highpass = x_i_highpass;
@@ -352,10 +352,10 @@ float signalFiltering(float dt, float x_i_lowpass)
 
 unsigned int measureRate(uint32_t clk_frequency)
 {
-	static uint64_t measurement_time = 0; // time of measurement
-	static bool began_measurement = false; // control whether measurement began
+	static uint64_t measuring_time = 0; // time of measuring
+	static bool began_measuring = false; // control whether measuring began
 
-	static float measured_values_arr[MEASURED_VALUES_ARR_SIZE] = {0.0};
+	static float measured_values_arr[3];
 	static size_t measured_values_arr_index = 0; // index to array of measured values
 
 	static float max_signal_value = 0.0; // maximum of signal
@@ -377,9 +377,9 @@ unsigned int measureRate(uint32_t clk_frequency)
 
 	uint32_t module_signal = ADC16_GetChannelConversionValue(ADC0, 0);
 
-	// time interval dt between measurements in microseconds
+	// time interval dt between measuring in microseconds
 	uint64_t dt = COUNT_TO_USEC((uint64_t) LPTMR_GetCurrentTimerCount(LPTMR0), clk_frequency);
-	measurement_time = measurement_time + dt; // counting time if measure
+	measuring_time = measuring_time + dt; // counting time of measuring
 
 	// reset of LPTMR timer
 	LPTMR_StopTimer(LPTMR0);
@@ -391,7 +391,7 @@ unsigned int measureRate(uint32_t clk_frequency)
 	if (ascending_signal == true)
 	{
 		// finding maximum of signal
-		if (filtered_signal > max_signal_value && filtered_signal > 0.0)
+		if ((filtered_signal > max_signal_value) && (filtered_signal > 0.0))
 		{
 			max_signal_value = filtered_signal;
 		}
@@ -399,12 +399,12 @@ unsigned int measureRate(uint32_t clk_frequency)
 		{
 			if (max_signal_value != 0.0)
 			{
-				if (measurement_time != 0 && began_measurement == true)
+				if ((measuring_time != 0 )&& (began_measuring == true))
 				{
-					measured_value = (60.0 / ((float) measurement_time / 1000000.0)); // compute result in bpm
+					measured_value = (60.0 / ((float) measuring_time / 1000000.0)); // compute result in bpm
 					//measured_value = measured_value - 70.0; // control because of better accuracy
 					// ignore rates larger than max human bpm and below min human bpm
-					if (measured_value <= max_human_rate && measured_value >= min_human_rate)
+					if ((measured_value <= max_human_rate) && (measured_value >= min_human_rate))
 					{
 						// add value to array
 						measured_values_arr[measured_values_arr_index] = measured_value;
@@ -413,8 +413,8 @@ unsigned int measureRate(uint32_t clk_frequency)
 				}
 
 				ascending_signal = false;
-				began_measurement = true;
-				measurement_time = 0;
+				began_measuring = true;
+				measuring_time = 0;
 
 			}
 		}
@@ -429,25 +429,26 @@ unsigned int measureRate(uint32_t clk_frequency)
 
 	}
 
-	if (measured_values_arr_index == MEASURED_VALUES_ARR_SIZE) // array of measured values is full
+	if (measured_values_arr_index == 3) // array of measured values is full
 	{
-		measured_values_arr_index = 0;
 
 		int sum = 0;
 
-		for (int i = 0; i < MEASURED_VALUES_ARR_SIZE; i++)
+		for (int i = 0; i < 3; i++)
 		{
 			sum = sum + measured_values_arr[i];
 		}
 
-		// compute average measured value of MEASURED_VALUES_ARR_SIZE values
-		measured_average_value = sum / MEASURED_VALUES_ARR_SIZE;
+		// compute average measured value of 3 values
+		measured_average_value = (sum / 3);
 
 		// clean whole array
-		for (size_t i = 0; i < MEASURED_VALUES_ARR_SIZE; i++)
+		for (size_t i = 0; i < 3; i++)
 		{
 			measured_values_arr[i] = 0.0;
 		}
+
+		measured_values_arr_index = 0;
 	}
 
 	return measured_average_value;
@@ -466,7 +467,7 @@ int main()
 	LPTMRinit();
 	ADCinit();
 
-	// display 0 in the beginning of measurement:
+	// display 0 in the beginning of measuring:
 	GPIO_WritePinOutput(BOARD_INITPINS_DISPLAY_SEGMENT_A_GPIO, BOARD_INITPINS_DISPLAY_SEGMENT_A_PIN, 1);
 	GPIO_WritePinOutput(BOARD_INITPINS_DISPLAY_SEGMENT_B_GPIO, BOARD_INITPINS_DISPLAY_SEGMENT_B_PIN, 1);
 	GPIO_WritePinOutput(BOARD_INITPINS_DISPLAY_SEGMENT_C_GPIO, BOARD_INITPINS_DISPLAY_SEGMENT_C_PIN, 1);
