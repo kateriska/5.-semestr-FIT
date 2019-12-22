31d8b54ec4b1f3b29dc6015d02bf2d655d2881b8
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-12-22 23:59:12 +0100

    Add files via upload

---
+++ ITU/projISO/apiUser.php
@@ -0,0 +1,37 @@
+<?php
+
+header('Content-type:application/json');
+$file = "dataUser.json";
+
+function insertChangesUser($file)
+{
+  if (array_key_exists('user', $_POST) && array_key_exists('firstname', $_POST) && array_key_exists('lastname', $_POST) && array_key_exists('favouriteGenres', $_POST) && array_key_exists('interests', $_POST))
+  {
+
+    $user = $_POST['user'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $favouriteGenres = $_POST['favouriteGenres'];
+    $interests = $_POST['interests'];
+
+    $array = array(
+      "user" => $user,
+      "firstname" => $firstname,
+      "lastname" => $lastname,
+      "favouriteGenres" => $favouriteGenres,
+      "interests" => $interests,
+    );
+
+
+    $jsondata =  json_encode($array);
+    file_put_contents($file, $jsondata.PHP_EOL);
+ }
+
+
+ }
+
+
+  insertChangesUser($file);
+
+  $loaded_file = file_get_contents($file, true);
+  echo $loaded_file;
